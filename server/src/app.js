7c63ca07f07af4798e1118da67fbd8a63a442f65
Eternatus <120200880+Eternarus@users.noreply.github.com>
2024-12-13 17:11:42 +0500

    setup of the express js server

---
+++ server/src/app.js
@@ -0,0 +1,20 @@
+console.log('hello world');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('combined'));
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/status', (req, res) => {
+    res.send({
+         message: 'Hello World!!' 
+    });
+}); 
+
+app.listen(process.env.PORT || 8081)
